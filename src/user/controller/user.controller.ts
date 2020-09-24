d3764ec9f028866c1f99c4a022a3f28670fd41ef
Tran Le Anh Duy <duytla@smartosc.com>
2020-09-24 22:14:13 +0700

    feature/users: Handle Resful API Users

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+import { UserService } from '../services/user.service';
+
+@Controller('users')
+export class UserController {
+    constructor(
+        private userService: UserService
+    ) { }
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user)
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number): Observable<User> {
+        console.log(id)
+        return this.userService.findOne(id)
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll()
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id', ParseIntPipe) id: number): Observable<User> {
+        return this.userService.deleteOne(id)
+    }
+
+    @Put(':id')
+    updateOne(@Param('id', ParseIntPipe) id: number, @Body() user: User): Observable<User> {
+        return this.userService.updateOne(id, user)
+    }
+}
---
+++ src/user/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from, Observable } from 'rxjs';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.interface';
+
+@Injectable()
+export class UserService {
+
+    constructor(@InjectRepository(UserEntity) private readonly UserRepository: Repository<UserEntity>) { }
+
+    create(user: User): Observable<User> {
+        return from(this.UserRepository.save(user));
+    }
+
+    findOne(id: number): Observable<User> {
+        return from(this.UserRepository.findOne({ id }))
+    }
+
+    findAll(): Observable<User[]> {
+        return from(this.UserRepository.find());
+    }
+
+    deleteOne(id: number): Observable<any> {
+        return from(this.UserRepository.delete(id));
+    }
+
+    updateOne(id: number, user: User): Observable<any> {
+        return from(this.UserRepository.update(id, user));
+    }
+}
