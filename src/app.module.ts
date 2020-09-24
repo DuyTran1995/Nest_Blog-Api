fd4013faa098d339702dd7c928ad1503110fdda9
Tran Le Anh Duy <duytla@smartosc.com>
2020-09-24 23:47:38 +0700

    feature/config: Setup config nestjs app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { typeOrmConfig } from './config/typeorm.config';
+import configuration from './config/configuration';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfig } from './database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig
+    }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
